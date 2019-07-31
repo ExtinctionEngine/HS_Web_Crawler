5b908b7a06a7b65b3964e5d57d671689abbbfe52
ExtinctionEngine <qianlima777@gmail.com>
2019-07-31 21:12:48 +0930

    normal updates

--- core.py
+++ core.py
@@ -13,38 +13,38 @@ NUMBER_OF_THREADS = 8
 queue = Queue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
 
-#
-# # Create worker threads (will die when main exists)
-# def create_workers():
-#     for _ in range(NUMBER_OF_THREADS):
-#         t = threading.Thread(target=work)
-#         t.demon = True
-#         t.start()
-#
-#
-# # Do the next job in the queue
-# def work ():
-#     while True:
-#         url = queue.get()
-#         Spider.crawl_page(threading.current_thread().name, url)
-#         queue.task_done()
-#
-#
-# # Each queued links is a new job
-# def create_jobs():
-#     for link in file_to_set(QUEUE_FILE):
-#         queue.put(link)
-#     queue.join()
-#     crawl()
-#
-#
-# # Check if there are items in the queue, if so crawl them
-# def crawl():
-#     queued_links =  file_to_set(QUEUE_FILE)
-#     if len(queued_links) > 0:
-#         print(str(len(queued_links))) + ' links in the queue'
-#         create_jobs()
-#
-#
-# create_workers()
-# crawl()
+
+# Create worker threads (will die when main exists)
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+
+# Do the next job in the queue
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done()
+
+
+# Each queued links is a new job
+def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+    queue.join()  # to ensure that jobs are done one by one
+    crawl()
+
+
+# Check if there are items in the queue, if so crawl them
+def crawl():
+    queued_links = file_to_set(QUEUE_FILE)
+    if len(queued_links) > 0:
+        print(str(len(queued_links)) + ' links in the queue')
+        create_jobs()
+
+
+create_workers()
+crawl()
